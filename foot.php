3cf4412d4cd042bfdd67d44e1fdb6de3f8b56976
TaiNguyen297 <tainguyen29702@gmail.com>
2023-07-05 00:28:32 +0700

    clothes

---
+++ foot.php
@@ -0,0 +1,21 @@
+ <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <footer class="footer">
+
+    <div class="footer__box">
+                    <div class="grid" style="background-color:#212529">
+                    <a href="index.php"><img src="img/ClothesStore1.png" style="height: 80px;width:80px"></a>
+                        <p class="footer__text">Đại Học Thủy Lợi</p>
+                        <p class="footer__text">175 - Tây Sơn - Đống Đa - Hà Nội</p>
+                        
+                    </div>
+    </div>
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
